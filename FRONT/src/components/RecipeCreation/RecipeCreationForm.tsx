b1459f516b9bf77de6fa09f337cd49eeb63a358c
GuruMey <leshem.m@gmail.com>
2023-07-03 01:13:08 +0300

    added backend routes for favorites

--- FRONT/src/components/RecipeCreation/RecipeCreationForm.tsx
+++ FRONT/src/components/RecipeCreation/RecipeCreationForm.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import styles from "./RecipeCreationForm.module.css"
+import {useEffect, useState} from "react";
 
 
- export default function RecipeCreationForm() {
+export default function RecipeCreationForm() {
 
 
     // Initialise un état pour gérer le contenu de l'input qui va servir à ajouter le prochain ingrédient, étape ou tag :
@@ -17,9 +18,42 @@ import styles from "./RecipeCreationForm.module.css"
     const [steps, setSteps] = React.useState<string[]>([]);
     const [tags, setTags] = React.useState<string[]>([]);
 
+    const [formData, setFormData] = useState({});
 
-    //Définit des fonctions pour gérer les événements du formulaire :
-    const handleCoverPhotoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+    const handleSubmit = async (event: any) => {
+        event.preventDefault();
+
+        const { title, time } = event.target.elements;
+        const formData = {
+            title: title.value,
+            time: time.value,
+            coverPhoto: coverPhoto,
+            ingredients,
+            steps,
+            tags,
+        };
+
+        try {
+            const response = await fetch('api/createRecipe/:recipeId', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+
+            if (response.ok) {
+                console.log('Données enregistrées avec succès !');
+            } else {
+                console.error('Erreur lors de l\'enregistrement des données.');
+            }
+        } catch (error) {
+            console.error('Erreur lors de la communication avec le serveur.', error);
+        }
+    };
+
+    const CoverPhotoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files && event.target.files[0]) {
             setCoverPhoto(event.target.files[0]);
         }
@@ -46,75 +80,84 @@ import styles from "./RecipeCreationForm.module.css"
             <h2>Create a new recipe</h2>
             </div>
 
-            <div className={styles.form}>
-            {/*--------------RECIPE TITLE-------------------*/}
-            <div className={styles.recipe_title}>
-                <form className={styles.title_form}>
-                    <label> Recipe Title: </label>
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <div className={styles.recipe_title}>
+                    <label>Recipe Title:</label>
                     <input className={styles.input_medium} type="text" name="title" />
-                </form> </div>
+                </div>
 
-            {/*--------------TIME-------------------*/}
-            <div className={styles.recipe_time}>
-                <form className={styles.time_form}>
-                    <label> Recipe Time (min): </label>
+                <div className={styles.recipe_time}>
+                    <label>Recipe Time (min):</label>
                     <input className={styles.input_short} type="number" name="time" />
-                </form>
-            </div>
-            {/*--------------PHOTO-------------------*/}
-            <div className={styles.recipe_cover_photo}>
-                <form className={styles.cover_photo_form}>
-                    <label> Cover Photo: </label>
-                    <input type="file" name="cover-photo" accept="image/*" onChange={handleCoverPhotoChange} />
-                </form>
-            </div>
-            {/*--------------INGREDIENTS-------------------*/}
-            <div className={styles.recipe_ingredients}>
-                <form className={styles.ingredients_form}>
-                    <label> Ingredients: </label> <br/>
-                    <input className={styles.input_medium} value={ingredientInput} onChange={(e)=>{setIngredientInput(e.target.value)}} type="text" name="ingredients" />
-                    <button className={styles.form_add_secondary_button} type="button" onClick={addIngredient}>+</button>
-                </form>
-                <div className={styles.recipe_list}><ul>
-                    {ingredients.map((ingredient, index) => (
-                        <li key={index}>{ingredient}</li>
-                    ))}
-                </ul></div>
-            </div>
-            {/*--------------STEPS-------------------*/}
-            <div className={styles.recipe_steps}>
-                <form className={styles.steps_form}>
-                    <label> Steps: </label> <br/>
-                    <input className={styles.input_long} value={stepInput} onChange={(e)=>{setStepInput(e.target.value)}} type="text" name="steps" />
-                    <button className={styles.form_add_secondary_button} type="button" onClick={addStep}>+</button>
-                </form>
-                <div className={styles.recipe_list}><ul>
-                    {steps.map((step, index) => (
-                        <li key={index}>{step}</li>
-                    ))}
-                </ul></div>
-            </div>
-            {/*--------------TAGS-------------------*/}
-            <div className={styles.recipe_tags}>
-                <form className={styles.tags_form}>
-                    <label> Tags: </label>
-                    <input value={tagInput} onChange={(e)=>{setTagInput(e.target.value)}} type="text" name="tags" />
-                    <button className={styles.form_add_secondary_button} type="button" onClick={addTag}>+</button>
-                </form>
-                <div className={styles.recipe_list}><ul>
-                    {tags.map((tag, index) => (
-                        <li key={index}>{tag}</li>
-                    ))}
-                </ul></div>
-            </div>
-            </div>
-            {/*--------------BUTTON-------------------*/}
-            <div className={styles.button_container}>
-                <div className={styles.form_primary_button}>Save </div>
-                <div className={styles.form_primary_button}>Publish </div>
-            </div>
-            <br/><br/><br/><br/>
-        </div>
+                </div>
 
+                <div className={styles.recipe_cover_photo}>
+                    <label>Cover Photo:</label>
+                    <input type="file" name="cover-photo" accept="image/*" onChange={CoverPhotoChange} />
+                </div>
+
+                <div className={styles.recipe_ingredients}>
+                    <label>Ingredients:</label>
+                    <input
+                        className={styles.input_medium}
+                        value={ingredientInput}
+                        onChange={(e) => {
+                            setIngredientInput(e.target.value);
+                        }}
+                        type="text"
+                        name="ingredients"
+                    />
+                    <button className={styles.form_add_secondary_button} type="button" onClick={addIngredient}>
+                        +
+                    </button>
+                    <ul className={styles.recipe_list}>
+                        {ingredients.map((ingredient, index) => (
+                            <li key={index}>{ingredient}</li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className={styles.recipe_steps}>
+                    <label>Steps:</label>
+                    <input
+                        className={styles.input_long}
+                        value={stepInput}
+                        onChange={(e) => {
+                            setStepInput(e.target.value);
+                        }}
+                        type="text"
+                        name="steps"
+                    />
+                    <button className={styles.form_add_secondary_button} type="button" onClick={addStep}>
+                        +
+                    </button>
+                    <ul className={styles.recipe_list}>
+                        {steps.map((step, index) => (
+                            <li key={index}>{step}</li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className={styles.recipe_tags}>
+                    <label>Tags:</label>
+                    <input className={styles.input_medium} value={tagInput} onChange={(e) => setTagInput(e.target.value)} type="text" name="tags" />
+                    <button className={styles.form_add_secondary_button} type="button" onClick={addTag}>
+                        +
+                    </button>
+                    <ul className={styles.recipe_list}>
+                        {tags.map((tag, index) => (
+                            <li key={index}>{tag}</li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className={styles.button_container}>
+                    <button className={styles.form_primary_button} type="submit">
+                        Save
+                    </button>
+                    <div className={styles.form_primary_button}>Publish</div>
+                </div>
+            </form>
+        </div>
     );
 }
