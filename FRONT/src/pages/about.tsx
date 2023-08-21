8c997c5accae67571ff6aa0e10aa030fb1ba80fb
GuruMey <leshem.m@gmail.com>
2023-08-21 10:28:52 +0200

    added page about

---
+++ FRONT/src/pages/about.tsx
@@ -0,0 +1,17 @@
+export default function About() {
+    return <div>
+        <p className={"section"}>
+            <h2>About</h2>
+        </p>
+
+        <p className={"section"}>Welcome to DelishDish ! Where culinary passion meets digital innovation. We've designed a platform for food enthusiasts just like you—a place where you can not only share your treasured recipes with the world but also discover new favorites curated by our vibrant community. Whether you're a seasoned chef or someone just starting their culinary journey, our platform is tailored to give you the best recipe-sharing experience.</p>
+
+        <br/>
+
+        <p className={"section"}>Our unique feature allows you to not only post your own creations but also to explore and add recipes from fellow members to your personal favorites list. With every new recipe you encounter, expand your cooking horizons and gather a collection of dishes tailored to your taste. Every dish has a story, and here, we believe in connecting those tales to your kitchen.</p>
+
+        <br/>
+
+        <p className={"section"}>Join us in making DelishDish the ultimate destination for recipes and culinary exploration. Craft, share, favorite, and most importantly, enjoy the delicious journey!</p>
+    </div>
+}
